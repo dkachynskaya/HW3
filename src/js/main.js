eeddb249d39a40bc1427ca9dcb5448313ac38427
Daria <dariya.kachinskaya@ukr.net>
2020-12-24 22:33:47 +0200

    added slider = blocks 5-6

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    $('.banner-box__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-box__slider-btn banner-box__slider-btnprev"><img src="images/arrow_prev.png" alt=""></button>',
+        nextArrow: '<button class="banner-box__slider-btn banner-box__slider-btnnext"><img src="images/arrow_next.png" alt=""></button>',
+    });
+
+});
